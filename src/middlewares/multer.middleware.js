976d2c99f12c15652e6cae798c5cede297391600
Parth <garg2006parth@gmail.com>
2025-09-03 02:28:18 +0530

    feat: added multer middlware to upload files

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,2 +1,23 @@
 import multer from "multer";
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const filePath = path.join(__dirname,'my-uploads')
+    if(!fs.existsSync(filePath))
+    {
+        fs.mkdirSync(filePath,{recursive:true})
+    }
+    cb(null,filePath)
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+export const upload = multer({ storage: storage })
